1eded84542b656978dce221e5f255ffc59894c19
mahmudy9 <mhmudee@gmail.com>
2018-02-09 19:34:26 +0200

    adminlte

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin', function () {
+    return view('board');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
